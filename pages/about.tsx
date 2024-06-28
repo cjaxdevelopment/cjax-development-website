1bbf25f2c12690f8436a47a67352db757f5c9740
Chris Jackson <chris.jackson@cjaxdevelopment.com>
2024-06-27 20:56:26 -0400

    about me width max

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar';
 
 const About = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex flex-col items-center bg-gray-100">
       <Head>
         <title>About Me</title>
         <meta name="description" content="About me page" />
@@ -12,7 +12,7 @@ const About = () => {
 
       <Navbar />
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-4 sm:px-8 text-center">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-4 sm:px-8 text-center mt-12 max-w-screen-lg mx-auto">
         <h1 className="text-4xl sm:text-6xl font-bold">About Me</h1>
         <p className="mt-4 text-lg sm:text-xl leading-relaxed">
           I am a web developer with a passion for creating beautiful and functional websites. With a strong background in front-end technologies, I strive to deliver exceptional user experiences. I have experience working with HTML, CSS, JavaScript, and modern frameworks like React and Next.js.
